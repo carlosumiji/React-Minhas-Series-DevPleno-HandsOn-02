2a1b517f3d8be805e02e717be9e238753f358ddf
Carlos Henrique Umiji Dias <carlos.dias@MJV.net>
2019-08-05 18:19:29 -0300

    criação e alteraçoes iniciais do projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react';
+import Header from './Header'
+import axios from 'axios'
+import{
+  BrowserRouter as Router,
+  Route
+}from 'react-router-dom'
+
+const Home = () => {
+  return <h1>Home</h1>
+}
+const Generos = () =>{
+  return <h1>Generos</h1>
+}
 
 function App() {
+  const[data, setData] = useState({})
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData()
+    })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Header/>
+        <Route path='/' exact component={Home} />
+        <Route path='/generos' component={Generos} />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
